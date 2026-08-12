1349a92595610a7bc51aa5ab1d1782b7a2e2203a
sku <alexandre.hamad@hotmail.fr>
2026-08-12 13:38:02 +0200

    add 2 function for bot -> WIP

--- srcs/bonus/bot/bot.cpp
+++ srcs/bonus/bot/bot.cpp
@@ -12,12 +12,29 @@
 
 #include "../../../includes/Server.hpp"
 
+//   fonction checkbot qui sert a recuperer le msg pour voir si il declanche le trigger BOT <- ou autre nom a debattre mdr
+
+ //	 donc dans l'ordre elle regarde si le caractere '#' est present et il n'y a que des espace
+//   -> return dans si elle trouve car ce ne lui est pas destiner
+
+//
+//
+//
+//
+
 void CheckBot(int fd, std::string Destination, std::string Msg)
 {
-	std::string trigger = "BOT";
-	
 	if (Destination.find('#') != std::string::npos)
 		return;
+
+	std::string trigger = "BOT";
+	size_t start = Msg.find_first_not_of(" ");
+
+	if (start == std::string::npos)
+   		return;
+
+	std::string trim = Msg.substr(start);
+
 }
 
 std::string BuildBotReply(const std::string &msg)
