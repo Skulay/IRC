e737ad628bc707f2be098eef54130b410b651432
sku <alexandre.hamad@hotmail.fr>
2026-08-12 13:46:13 +0200

    add comment with logic and idea for reply bot

--- srcs/bonus/bot/bot.cpp
+++ srcs/bonus/bot/bot.cpp
@@ -17,10 +17,16 @@
  //	 donc dans l'ordre elle regarde si le caractere '#' est present et il n'y a que des espace
 //   -> return dans si elle trouve car ce ne lui est pas destiner
 
-//
-//
-//
-//
+//	 comparer la string trigger voir si on la trouve dans msg <- une comparaison sur la lenght en gros
+
+//	 renvoyer une string "reply" si tout est good -> on a besoin de savoir quoi rep 
+//	 -> dice (lancer aleatoire sur 6 ou 100 ?)
+//	 -> lancer de piece (resultat true/false 1/2)
+//	 -> ascii (renvoie un dessins ascii)
+//	 -> joke (on met des blague nul dans une struct de string et on en renvoie une au pif)
+//	 -> user (renvoie le nombre d'utilisateur sur le server)
+//	 -> ping (repond pong mdr)
+//	 -> 
 
 void CheckBot(int fd, std::string Destination, std::string Msg)
 {
