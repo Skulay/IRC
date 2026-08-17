a7c8e6089a7cce98b381d98ecfa7bf5d2f66a409
sku <alexandre.hamad@hotmail.fr>
2026-08-17 12:32:13 +0200

    need to work on mode

--- srcs/command/pass.cpp
+++ srcs/command/pass.cpp
@@ -6,7 +6,7 @@
 /*   By: alehamad <alehamad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/10 15:43:54 by amkhelif          #+#    #+#             */
-/*   Updated: 2026/08/17 12:06:51 by alehamad         ###   ########.fr       */
+/*   Updated: 2026/08/17 12:11:11 by alehamad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,26 +18,23 @@ void Server::ExecutePass(int fd, std::string Argv)
 {
     if (Argv.empty())
     {
-        std::string msg = ":ircserv 461 * PASS :Not enough parameters\r\n";
+        std::string msg = ":irc.local 461 * PASS :Not enough parameters\r\n";
         send(fd, msg.c_str(), msg.length(), 0);
         return;
     }
     if (_Client[fd].GetPassClient()) // si le client a deja valide son mdp
     {
-        std::string err = ":ircserv 462 * :You may not reregister\r\n";
+        std::string err = ":irc.local 462 * :You may not reregister\r\n";
         send(fd, err.c_str(), err.length(), 0);
         return;
     }
-    // std::cout << std::endl << this->_PassWord << this->_PassWord.length() << Argv << Argv.length() << std::endl;
-    std::cout << this->_PassWord.length() << this->_PassWord << std::endl;
-    std::cout << Argv.length() << Argv << std::endl;
     if (this->_PassWord == Argv) // si le texte corspond au mdp du serveur c bon
     {
         _Client[fd].SetPass(true);
     }
     else // so le mdp nes pas celui du serveur
     {
-        std::string msg = ":ircserv 464 * :Password incorrect\r\n";
+        std::string msg = ":irc.local 464 * :Password incorrect\r\n";
         send(fd, msg.c_str(), msg.length(), 0);
         DisconnectClient(fd, "Password incorrect");
     }
